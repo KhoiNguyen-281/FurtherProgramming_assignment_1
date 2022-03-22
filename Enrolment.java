448304f9310cde8958e44c0547b11f728e656664
nguyenhoangminhkhoi <nguyenkhoi28100@gmail.com>
2022-03-22 20:41:16 +0700

    v3.22.3 added addEnrolment, modified addSemester, dropCourse, class Enrolment

--- Enrolment.java
+++ Enrolment.java
@@ -1,34 +1,33 @@
 public class Enrolment {
-    private Student student;
+    private String student;
     private String semester;
-    private Course course;
+    private String course;
 
-    public Enrolment(Student student, String semester, Course course) {
+    public Enrolment(String student, String semester, String course) {
         this.student = student;
         this.semester = semester;
         this.course = course;
     }
 
-    public String getStuID() {
-        String stuID = student.getStudentID();
-        return stuID;
+
+    public String getStudent() {
+        return student;
     }
 
     public String getSemester() {
         return semester;
     }
 
-    public String getCouID() {
-        String couID = course.getCourseID();
-        return couID;
+    public String getCourse() {
+        return course;
     }
 
     @Override
     public String toString() {
         return "Enrolment{" +
-                "student=" + getStuID() +
-                ", semester='" + semester + '\'' +
-                ", course=" + getCouID() +
+                "student: " + student +
+                ", semester: '" + semester + '\'' +
+                ", course: " + course +
                 '}';
     }
 }
